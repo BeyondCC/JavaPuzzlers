73b7f92a8d01a771e31476dec4d97fb95160ed88
BeyondCC <cclovedota@gmail.com>
2014-03-14 00:15:47 +0800

    The last laugh

---
+++ src/com/cc/ch3/PuzzlersWithCharacter/TheLastLaugh11.java
@@ -0,0 +1,16 @@
+package com.cc.ch3.PuzzlersWithCharacter;
+
+public class TheLastLaugh11 {
+
+	public static void main(String[] args) {
+		System.out.println("H" + "A");
+		System.out.println('H' + 'a');
+		
+		System.out.println(" 2 + 2 = " + 2 + 2 );
+		System.out.println("" + 'H' + 'a');
+		
+		//char 'H' and 'a' cast to int via zero extension
+		//widen primitive conversion
+	}
+
+}
